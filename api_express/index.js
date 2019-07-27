d25a6e07bd84c4324c6bd81181081ed08d84f989
Fernando Gomez Perera <160300129@ucaribe.edu.mx>
2019-07-27 15:17:00 -0500

    Express

--- api_express/index.js
+++ api_express/index.js
@@ -1,3 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-console.log(express)
+app.use(bodyParser.json());
+app.get('/user', (req, res) => {
+    const user = {
+        username: "foo",
+        id: 222
+    }
+    console.log(req.body);
+    res.send(JSON.stringify(user));
+})
+
+app.post('/user', (req, res) => {
+    console.log(req.body);
+})
+
+app.listen(3002, () => {
+    console.log('Servidor corriendo....');
+})
